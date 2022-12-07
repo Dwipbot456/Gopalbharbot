464076c737031085bc697c7d98d514743d8b5cc9
Dwipbot456 <119711820+Dwipbot456@users.noreply.github.com>
2022-12-07 09:58:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -31,5 +31,5 @@ class Config(object):
     AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 300))
     MDISK_API = os.getenv("MDISK_API", "12334")
     VERIFIED_TIME  = int(os.getenv("VERIFIED_TIME", "1"))
-    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT")
+    ABOUT_BOT_TEXT = os.getenv("ABOUT_TEXT",ғᴏʀ ᴍᴏʀᴇ ɪɴғᴏ @ROYALDWIP)
     ABOUT_HELP_TEXT = os.getenv("HELP_TEXT")
